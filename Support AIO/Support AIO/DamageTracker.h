b2870ead42a0366cb8a9752523dabd689835a7cd
Korn1s <kornelijus142@inbox.lt>
2017-04-06 19:30:08 +0300

    Add files via upload

--- Support AIO/Support AIO/DamageTracker.h
+++ Support AIO/Support AIO/DamageTracker.h
@@ -1,23 +1,22 @@
 #pragma once
-#include "PluginSDK.h"
-#include <algorithm>
 #include <unordered_map>
+#include "PluginSDK.h"
 
 class IUnit;
 
 class DamageTracker
 {
 public:
-    explicit DamageTracker(IPluginSDK *sdk, bool trackSkillshots = true);
+	explicit DamageTracker(IPluginSDK *sdk, bool trackSkillshots = true);
 
-    float getIncomingDamage(IUnit *unit) const;
-    float getIncomingDamagePercent(IUnit *unit) const;
-    bool hasIncomingDamage(IUnit *unit) const;
-    bool hasIncomingLethalDamage(IUnit *unit, float &extraDamage) const;
+	float getIncomingDamage(IUnit *unit) const;
+	float getIncomingDamagePercent(IUnit *unit) const;
+	bool hasIncomingDamage(IUnit *unit) const;
+	bool hasIncomingLethalDamage(IUnit *unit, float &extraDamage) const;
 
 private:
-    IPluginSDK *sdk;
-    bool trackSkillshots;
+	IPluginSDK *sdk;
+	bool trackSkillshots;
 
-    std::unordered_map<int, float> damage;
+	std::unordered_map<int, float> damage;
 };
