6afcdc95f78f7a9ca4a88b2c51b5ac7df76f651d
Korn1s <kornelijus142@inbox.lt>
2017-05-06 20:37:13 +0300

    Update Support AIO.cpp

--- Support AIO/Support AIO/Support AIO.cpp
+++ Support AIO/Support AIO/Support AIO.cpp
@@ -1276,7 +1276,7 @@ public:
 		VelKozBase().AutoE();
 		VelKozBase().AAdisable();
 		VelKozBase().autoQ();
-		//VelKozBase().setQ();
+		VelKozBase().setQ();
 		VelKozBase().Killsteal();
 		if (GOrbwalking->GetOrbwalkingMode() == kModeCombo)
 		{
